4b3adffd9fe3293a7a8550c3424e26b4511672e9
Leejinyeong <yago0315@gmail.com>
2017-10-31 22:46:28 +0900

    Modify TabLayout(Equalizer)

--- MyApplication/app/src/main/java/com/example/dsm2016/androideq/EqualizerActivity.java
+++ MyApplication/app/src/main/java/com/example/dsm2016/androideq/EqualizerActivity.java
@@ -157,6 +157,8 @@ public class EqualizerActivity extends AppCompatActivity {
             }
         });
 
+
+
     }
 
 
--- MyApplication/app/src/main/java/com/example/dsm2016/androideq/TabLayout2.java
+++ MyApplication/app/src/main/java/com/example/dsm2016/androideq/TabLayout2.java
@@ -1,24 +1,179 @@
 package com.example.dsm2016.androideq;
 
-import android.content.Intent;
+import android.media.audiofx.Equalizer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
  * Created by dsm2016 on 2017-10-24.
  */
 
 public class TabLayout2 extends Fragment{
+
+    Equalizer equalizer;
+
+    SeekBar seekBar1;
+    SeekBar seekBar2;
+    SeekBar seekBar3;
+    SeekBar seekBar4;
+    SeekBar seekBar5;
+
+    TextView text1;
+    TextView text2;
+    TextView text3;
+    TextView text4;
+    TextView text5;
+
+    TextView eqInfo1;
+    TextView eqInfo2;
+    TextView eqInfo3;
+    TextView eqInfo4;
+    TextView eqInfo5;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        Intent intent = new Intent(getActivity(),EqualizerActivity.class);
 //        startActivity(intent);
         View v = inflater.inflate(R.layout.activity_equalizer,container,false);
+
+
+        try{
+            equalizer = null;
+            equalizer = new Equalizer(0, 0);
+            equalizer.setEnabled(true);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        Log.i("1 : ", equalizer.getBandLevel((short)0) + " " + equalizer.getBandLevel((short)1) + " " +
+                equalizer.getBandLevel((short)2) + " " + equalizer.getBandLevel((short)3) + " " + equalizer.getBandLevel((short)4));
+
+        seekBar1 = (SeekBar)v.findViewById(R.id.seekBar1);
+        seekBar2 = (SeekBar)v.findViewById(R.id.seekBar2);
+        seekBar3 = (SeekBar)v.findViewById(R.id.seekBar3);
+        seekBar4 = (SeekBar)v.findViewById(R.id.seekBar4);
+        seekBar5 = (SeekBar)v.findViewById(R.id.seekBar5);
+
+        text1 = (TextView)v.findViewById(R.id.text1);
+        text2 = (TextView)v.findViewById(R.id.text2);
+        text3 = (TextView)v.findViewById(R.id.text3);
+        text4 = (TextView)v.findViewById(R.id.text4);
+        text5 = (TextView)v.findViewById(R.id.text5);
+
+        eqInfo1 = (TextView)v.findViewById(R.id.eqInfo1);
+        eqInfo2 = (TextView)v.findViewById(R.id.eqInfo2);
+        eqInfo3 = (TextView)v.findViewById(R.id.eqInfo3);
+        eqInfo4 = (TextView)v.findViewById(R.id.eqInfo4);
+        eqInfo5 = (TextView)v.findViewById(R.id.eqInfo5);
+
+        seekBar1.setProgress(equalizer.getBandLevel((short)0));
+        seekBar2.setProgress(equalizer.getBandLevel((short)1));
+        seekBar3.setProgress(equalizer.getBandLevel((short)2));
+        seekBar4.setProgress(equalizer.getBandLevel((short)3));
+        seekBar5.setProgress(equalizer.getBandLevel((short)4));
+
+        text1.setText(String.valueOf(seekBar1.getProgress()));
+        text2.setText(String.valueOf(seekBar2.getProgress()));
+        text3.setText(String.valueOf(seekBar3.getProgress()));
+        text4.setText(String.valueOf(seekBar4.getProgress()));
+        text5.setText(String.valueOf(seekBar5.getProgress()));
+
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                equalizer.setBandLevel((short)0, (short)(i-1500));
+                text1.setText(String.valueOf(equalizer.getBandLevel((short)0)));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                equalizer.setBandLevel((short)1, (short)(i-1500));
+                text2.setText(String.valueOf(equalizer.getBandLevel((short)1)));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                equalizer.setBandLevel((short)2, (short)(i-1500));
+                text3.setText(String.valueOf(equalizer.getBandLevel((short)2)));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar4.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                equalizer.setBandLevel((short)3, (short)(i-1500));
+                text4.setText(String.valueOf(equalizer.getBandLevel((short)3)));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar5.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                equalizer.setBandLevel((short)4, (short)(i-1500));
+                text5.setText(String.valueOf(equalizer.getBandLevel((short)4)));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+
         return v;
     }
 }
